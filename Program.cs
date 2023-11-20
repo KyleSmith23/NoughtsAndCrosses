e6d8ecfafa1e4a92200c6283ab0dc1b5a08c44b5
kyle- <kyle-smith1@outlook.com>
2023-11-20 18:21:44 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+Board board = new();
+board.CreateBoard();
+
+
+// Classes
+public class Board
+{
+
+    string[] topRow = { " ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " " };
+    string[] midRow = { " ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " " };
+    string[] botRow = { " ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " " };
+    string[] dashRow = { "-", "-", "-", "+", "-", "-", "-", "+", "-", "-", "-", };
+
+    public void CreateBoard()
+    {
+        foreach (string position in topRow) { Console.Write(position); }
+        Console.WriteLine();
+        foreach (string dash in dashRow) { Console.Write(dash); }
+        Console.WriteLine();
+        foreach (string position in midRow) { Console.Write(position); }
+        Console.WriteLine();
+        foreach (string dash in dashRow) { Console.Write(dash); }
+        Console.WriteLine();
+        foreach (string position in botRow) { Console.Write(position); }
+    }
+   
+
+}
